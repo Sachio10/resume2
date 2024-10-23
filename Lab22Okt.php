b94a394e4b1a91dfcdda9c96feec1dba973581a0
Sachio10 <hh6079435@gmail.com>
2024-10-23 20:22:49 +0800

    Add files via upload

---
+++ Lab22Okt.php
@@ -0,0 +1,47 @@
+
+<?php
+    header('Content-Type: text/plain');
+    
+    $resume = <<<EOD
+    MOHAMMAD HAFIZUDDIN BIN MERAN
+    Kuching, Sarawak | (+60) 14-6960446 | hh6079435@gmail.com
+    
+    SUMMARY
+    As a full-time student at Polytechnic Kuching, Sarawak, I am recognized for my leadership, teamwork, and critical thinking skills. 
+    I excel at guiding teams through challenges with innovative solutions and strategic planning. 
+    I am actively seeking a 5-month internship from January to May 2025 to learn and grow my knowledge through real-world experiences. 
+    This opportunity will enable me to apply my leadership and collaboration abilities to impactful projects while further expanding my expertise in Information Technology.
+    
+    EDUCATION
+    Polytechnic Kuching, Kuching, Sarawak
+    Diploma in Information Technology (Digital Technology) | CGPA: 3.88/4.00          August 2022 – Current
+    - Specialization: Software and Applications Development.
+    - Relevant Coursework: Python Programming, Visual Basic Programming, JavaScript, Web Design Technology, C++.
+    
+    SMK SEMERAH PADI, Kuching, Sarawak
+    Malaysian Certificate of Education (SPM) | January 2017 – March 2022
+    
+    AWARDS AND ACHIEVEMENTS
+    - Head of Department's Award recipient for 4 consecutive semesters.
+    - Two times 4.00 GPA achiever.
+    
+    EXTRACURRICULAR INVOLVEMENTS
+    - Football Team Member: Developed strong teamwork, leadership, and physical fitness through regular practice and matches.
+    - Badminton Club Member: Actively play badminton almost every day, enhancing physical capabilities and fitness skills.
+    - PolyCC LoL: Participated in the Large Language Model Competition and gained new information regarding datasets.
+    
+    SKILLS
+    - Programming Languages: C++ (Intermediate), Java (Intermediate), Python (Advanced), VB.NET (Intermediate), HTML (Intermediate), CSS (Intermediate), JavaScript (Intermediate).
+    - Technical Skills: Kali Linux (Beginner), VS Code (Advanced), Visual Studio (Advanced), MySQL (Intermediate), Adobe Photoshop (Amateur), Adobe Premiere Pro (Beginner), Adobe Animate (Amateur), Adobe Audition (Beginner), Google Appsheet (Beginner), Android Studio (Amateur), Flutter Flow (Amateur), Firebase (Amateur).
+    - Languages: Malay (Native), English (Bilingual Proficiency).
+    
+    REFERENCE
+    Academic Advisor: Nur Yahzelina Hanafi
+    Pegawai Pendidikan Tinggi (PPT), Polytechnic Kuching, Sarawak
+    Phone: (+60) 19-264 9619 | Email: yahzelina@poliku.edu.my
+    
+    EOD;
+
+    echo $resume;
+?>
+    
